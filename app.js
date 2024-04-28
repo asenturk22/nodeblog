3c3809ef7b3a4777cbb97c858327b34e4acbf9be
Abdullah Şentürk <asenturk22@yandex.com>
2024-04-28 10:52:05 +0300

    nodeblog | app.js deki yonlendirmeler main.js' e aktarildi ve Add Post sayfasi olusturuldu

--- app.js
+++ app.js
@@ -8,41 +8,17 @@ const app = express()
 const hostname = "127.0.0.1"
 const port = 3000
 
-mongoose.connect('mongodb://127.0.0.1:27017/nodeblog_db')
-  .then(() => console.log('Connected!'));
 
 // View engine setup 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', handlebars.engine())
 app.set('views', './views')
 
+ app.use(express.static("public"))   
 
-app.use(express.static("public"))   
+const main = require('./routes/main')
+app.use('/', main)
 
-
-app.get('/', (req, res) => {
-    res.render('site/index');
-})
-
-app.get('/about', (req, res) => {
-    res.render('site/about');
-})
-
-app.get('/blog', (req, res) => {
-    res.render('site/blog');
-})
-
-app.get('/contact', (req, res) => {
-    res.render('site/contact');
-})
-
-app.get('/login', (req, res) => {
-    res.render('site/login');
-})
-
-app.get('/register', (req, res) => {
-    res.render('site/register');
-})
 
 app.listen(port, hostname,  () => {
     console.log(` Server calisiyor, http://${hostname}:${port}/`)
